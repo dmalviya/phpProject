7cf05de36000c92688ad36797ad52f57216e346e
dmalviya <30290365+dmalviya@users.noreply.github.com>
2018-01-16 18:24:00 +0530

    source upgrade

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>PHP+HTML+CSS</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script src="jquery/jquery-3.2.1.js"></script>
+    <script src="jquery/myjquery.js"></script>
+  </head>
+  <body>
+    <?php echo "<h1>It's working with css </h1>"; ?>
+  </body>
+</html>
---
+++ jquery/myjquery.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  $("h1").css("font-size","55px");
+  $("h1").fadeOut("slow");
+});
